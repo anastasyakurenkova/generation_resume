27a47a0e9512c64e69ff9fbd7833d55cdb18d9c9
Куренкова Анастасия Алексеевна <aakurenkova_5@edu.hse.ru>
2024-02-19 18:37:14 +0300

    Controversial remake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
     with open('resume.txt', 'w') as f:
         f.write('Имя: Винт')
+        f.write('Место работы: МИЭМ НИУ ВШЭ')
 if __name__ == "__main__":
-    main()
+    main()
+
+
