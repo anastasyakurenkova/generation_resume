41c8eacabe42adfcb891a223ddfa7ae8b9619fc7
Куренкова Анастасия Алексеевна <aakurenkova_5@edu.hse.ru>
2024-02-20 15:10:53 +0300

    aftermerge

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     with open('resume.txt', 'w') as f:
         f.write('Имя: Винт')
         f.write('Текущая должность: инженер эксплуатации')
+        f.write('Место работы: МИЭМ НИУ ВШЭ')
 if __name__ == "__main__":
     main()
 
